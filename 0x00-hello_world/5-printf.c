cd3419ad9ee2855bb776e10b99614b4e8b29598a
Gerishon Wachira <shongmagachi@gmail.com>
2022-09-08 19:35:17 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'Use function printf to print string'
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
