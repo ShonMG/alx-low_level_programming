e4f21e6bdf5b7e7d6d3cb1d10be5df720f5d17f9
Gerishon Wachira <shongmagachi@gmail.com>
2022-09-27 00:04:37 -0700

    first task complete

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/**
+ * _memset - fills the first n bytes of the memory
+ * area pointed to by s with constant byte b
+ * Return: a pointer to memory area s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; c < n; c++)
+		s[c] = b;
+	return (s);
+}
