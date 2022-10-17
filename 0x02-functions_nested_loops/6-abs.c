a39d6d3fbe1d81812e47455ce84cb8e479e753a2
Gerishon Wachira <shongmagachi@gmail.com>
2022-10-17 02:16:44 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of integer
+ * @i: integer to be converted
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
