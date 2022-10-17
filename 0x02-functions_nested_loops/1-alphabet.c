9ed13924ea609f9ac6cdd7ebf7ff0a429953ff43
Gerishon Wachira <shongmagachi@gmail.com>
2022-10-17 01:15:24 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		_putchar(start);
+	}
+	_putchar('\n');
+}
