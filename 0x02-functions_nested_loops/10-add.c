f36dee510a782c86947c56cfb77cc247b12a3c4b
Gerishon Wachira <shongmagachi@gmail.com>
2022-10-17 03:01:18 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @i - first number to add
+ * @j - second number to add
+ * Return: result of sum
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
